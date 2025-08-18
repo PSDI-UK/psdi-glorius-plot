58c1f92bd4ac9718329aa0091a0bb7a6486724cd
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2025-08-18 14:16:21 +0100

    Finish first pass at static rendering script

--- psdi_reaction_radar/render_static.py
+++ psdi_reaction_radar/render_static.py
@@ -4,7 +4,12 @@ This script renders all pages in the site as static html pages.
 """
 
 import os
+import shutil
 from argparse import ArgumentParser
+
+import psdi_reaction_radar
+from psdi_reaction_radar.gui.env import update_env
+from psdi_reaction_radar.gui.get import d_pages
 
 DEFAULT_TARGET_DIR = "public"
 
@@ -19,13 +24,49 @@ def main():
                         help="The desired directory (absolute or relative to where this script is run from) to render"
                         "the site to")
 
+    parser.add_argument("--use-env-vars", action="store_true",
+                        help="If set, all other arguments and defaults for this script are ignored, and environmental "
+                        "variables and their defaults will instead control execution. These defaults will result in "
+                        "the app running in production server mode.")
+
+    parser.add_argument("--service-mode", action="store_true",
+                        help="If set, will run as if deploying a service rather than the local GUI")
+
+    parser.add_argument("--dev-mode", action="store_true",
+                        help="If set, will expose development elements, such as the SHA of the latest commit")
+
+    parser.add_argument("--debug", action="store_true",
+                        help="If set, will run the Flask server in debug mode, which will cause it to automatically "
+                        "reload if code changes and show an interactive debugger in the case of errors")
+
+    parser.add_argument("--log-level", type=str, default=None,
+                        help="The desired level to log at. Allowed values are: 'DEBUG', 'INFO', 'WARNING', 'ERROR, "
+                             "'CRITICAL'. Default: 'INFO' for logging to file, 'WARNING' for logging to stdout")
+
     args = parser.parse_args()
+
+    if not args.use_env_vars:
+        # Overwrite the values from environmental variables with the values from the command-line arguments
+        update_env(args)
 
     # Ensure the output directory exists
     output_dir = os.path.abspath(args.output)
     os.makedirs(output_dir, exist_ok=True)
     if not os.path.isdir(output_dir):
         raise FileNotFoundError(f"Unable to create directory {output_dir}")
+
+    # Get the location of the project's root directory
+    project_dir = os.path.abspath(os.path.join(psdi_reaction_radar.__path__[0], ".."))
+
+    # Copy over the static contents directly to the output directory
+    shutil.copytree(os.path.join(project_dir, "static"), output_dir)
+
+    # Render all pages and output them to the output directory
+    for path, func in d_pages.items():
+        if not path.endswith(".html"):
+            continue
+        qualified_path = os.path.join(output_dir, path)
+        open(qualified_path, "w").write(func())
 
 
 if __name__ == "__main__":
