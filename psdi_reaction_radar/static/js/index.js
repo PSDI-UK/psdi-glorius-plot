0eede27b82eb1e85a30890740f1671222c1218ed
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2025-09-04 11:06:26 +0100

    Implement adding sample columns

--- psdi_reaction_radar/static/js/index.js
+++ psdi_reaction_radar/static/js/index.js
@@ -12,6 +12,9 @@ import { mix_hexes } from "./color.js"
 const MIN_CONDITIONS = 3;
 const MAX_CONDITIONS = 12;
 
+const MIN_SAMPLES = 1;
+const MAX_SAMPLES = 10;
+
 const MIN_OUTPUTS = 1;
 const MAX_OUTPUTS = 5;
 
@@ -62,6 +65,10 @@ function enableButton(button) {
 
 function updateConditionSelector() {
   $("select#num-conditions").val(getNumConditions()).change();
+}
+
+function updateSampleSelector() {
+  $("select#num-samples").val(getNumSamples()).change();
 }
 
 function updateOutputSelector() {
@@ -229,21 +236,136 @@ function relableSampleCols() {
   const lSampleHeadings = $(".value-heading");
 
   for (let k = 0; k < numSamples; k++) {
-    const sampleNumberString = k.toString();
 
     // Set the heading text
     let headingText;
     if (numSamples == 1) {
       headingText = "Value";
     } else {
-      headingText = "Sample " + sampleNumberString;
+      headingText = "Sample " + (k + 1).toString();
     }
     lSampleHeadings.eq(k).text(headingText);
 
     // Fix the IDs of the buttons
     const buttonCell = lSampleButtonCells.eq(k);
+    const sampleNumberString = k.toString();
     buttonCell.find(".remove-sample").attr("id", "remove-sb-" + sampleNumberString);
     buttonCell.find(".add-sample").attr("id", "add-sb-" + sampleNumberString);
+  }
+}
+
+function addSampleCol(e, updateAfter = true) {
+
+  // Check that we don't already have too many samples
+  const numSamples = getNumSamples();
+  if (numSamples >= MAX_SAMPLES) {
+    console.error("Attempt to add sample when maximum samples already reached");
+    return;
+  }
+
+  // Determine where to add the column based on which button was clicked
+  let targetCol;
+  if (e === null) {
+    targetCol = numSamples - 1;
+  } else {
+    targetCol = getIndexFromEvent(e);
+  }
+
+  // Construct and insert a new heading cell and baseline value cell
+  const newHeadingCell = $(".value-heading")[0].cloneNode(true);
+  const newBaselineValueCell = $(".baseline-value-cell")[0].cloneNode(true);
+
+  if (targetCol >= numSamples - 1) {
+    $(".sensitivity-header")[0].insertBefore(newHeadingCell, $(".deviation-heading")[0]);
+    $(".baseline-row")[0].insertBefore(newBaselineValueCell, $(".baseline-deviation-cell")[0]);
+  } else {
+    $(".sensitivity-header")[0].insertBefore(newHeadingCell, $(".value-heading")[targetCol + 1]);
+    $(".baseline-row")[0].insertBefore(newBaselineValueCell, $(".baseline-value-cell")[targetCol + 1]);
+  }
+
+  // For each row of the table, construnct and insert a new value cell
+  const numConditions = getNumConditions();
+  for (let i = 0; i < numConditions; i++) {
+    const conditionRow = $(".condition-row")[i];
+    const lValueCells = $(conditionRow).find(".sample-value-cell");
+    const newValueCell = lValueCells[0].cloneNode(true);
+
+    if (targetCol >= numSamples - 1) {
+      conditionRow.insertBefore(newValueCell, $(conditionRow).find(".deviation-value-cell")[0]);
+    } else {
+      conditionRow.insertBefore(newValueCell, lValueCells[targetCol + 1]);
+    }
+  }
+
+  // Check if we've reached the maximum number of samples, and disable the button to add samples if so
+  if (numSamples + 1 >= MAX_SAMPLES) {
+    disableButton($("button.add-sample"));
+  }
+
+  // Check if we've passed the minimum number of samples, and enable the button to remove samples if so
+  if (numSamples + 1 > MIN_SAMPLES) {
+    enableButton($("button.remove-sample"));
+  }
+
+  // Enable auto updates for new cells if it's turned on
+  if (autoUpdating) {
+    enableAutoUpdates();
+  }
+
+  // Update affected properties if desired at this point
+  if (updateAfter) {
+    updateSampleSelector();
+    relableSampleCols();
+    initNumSampleControls();
+  }
+
+  // Update the plot if desired
+  if (updateAfter && autoUpdating) {
+    generatePlot();
+  }
+}
+
+function removeSampleCol(e, updateAfter = true) {
+
+  // Check that we don't already have too few rows
+  const numConditions = getNumConditions();
+  if (numConditions <= MIN_CONDITIONS) {
+    console.error("Attempt to remove row when minimum rows already reached");
+    return;
+  }
+
+  // Determine which row to remove based on which button was clicked
+  let targetRow;
+  if (e === null) {
+    targetRow = numConditions - 1;
+  } else {
+    targetRow = getIndexFromEvent(e);
+  }
+
+  const sensTable = $(".sensitivity-table tbody")[0];
+  const lRows = $(".condition-row");
+  sensTable.removeChild(lRows[targetRow]);
+
+  // Check if we've reached the minimum number of rows, and disable the buttons to remove rows if so
+  if (numConditions - 1 <= MIN_CONDITIONS) {
+    disableButton($(".remove-condition"));
+  }
+
+  // Check if we've gotten under the minimum number of rows, and enable the buttons to add rows if so
+  if (numConditions - 1 < MAX_CONDITIONS) {
+    enableButton($(".add-condition"));
+  }
+
+  // Update affected properties if desired at this point
+  if (updateAfter) {
+    updateConditionSelector();
+    relableConditionRows();
+    initNumConditionControls();
+  }
+
+  // Update the plot if desired
+  if (updateAfter && autoUpdating) {
+    generatePlot();
   }
 }
 
@@ -938,8 +1060,8 @@ function initNumSampleControls() {
   $("button.remove-sample").off("click");
   $("select#num-sample").off("change");
 
-  $("button.add-sample").on("click", (e) => addSample(e, true));
-  $("button.remove-sample").on("click", (e) => removeSample(e, true));
+  $("button.add-sample").on("click", (e) => addSampleCol(e, true));
+  $("button.remove-sample").on("click", (e) => removeSampleCol(e, true));
   $("select#num-sample").on("change", (e) => setNumSamples($(e.target).val()));
 }
 
