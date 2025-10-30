02c5d12b70c9db92ae20c99cc71f4c2899dc08dd
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2025-10-30 15:10:25 +0000

    Fix concentration abbreviation

--- psdi_glorius_plot/static/js/index.js
+++ psdi_glorius_plot/static/js/index.js
@@ -56,7 +56,7 @@ const D_COLOR_SCHEMES = {
 
 };
 
-const CONDITION_PLACEHOLDER = "Condition (e.g. “High c”)";
+const CONDITION_PLACEHOLDER = "Condition (e.g. “High conc.”)";
 
 const DEFAULT_VALUE_MEAN = 100;
 const VALUE_MIN = 0.;
