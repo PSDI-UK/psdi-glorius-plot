dfd9cb3df2644353af9f18e627af0a5ea08cd5ab
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2025-10-30 15:40:40 +0000

    Add "e.g." to placeholders

--- psdi_glorius_plot/static/js/index.js
+++ psdi_glorius_plot/static/js/index.js
@@ -56,7 +56,7 @@ const D_COLOR_SCHEMES = {
 
 };
 
-const CONDITION_PLACEHOLDER = "“High conc.”";
+const CONDITION_PLACEHOLDER = "e.g. “High conc.”";
 
 const DEFAULT_VALUE_MEAN = 100;
 const VALUE_MIN = 0.;
@@ -1976,7 +1976,7 @@ function enableQuillEvents() {
 }
 
 function initQuill() {
-  addQuillEditor("#title-input", "“Reaction-condition sensitivity analysis”");
+  addQuillEditor("#title-input", "e.g. “Reaction-condition sensitivity analysis”");
   addQuillEditor("#ol-0", "Define outcome");
   $(".condition-input").each((i, e) => {
     addQuillEditor("#cl-" + i, CONDITION_PLACEHOLDER);
