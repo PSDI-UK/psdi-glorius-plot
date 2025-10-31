7706945299cf15923f4cb59d3a4333433393258c
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2025-10-31 13:35:34 +0000

    Add documentation and new function

--- psdi_glorius_plot/static/js/dirty-forms.js
+++ psdi_glorius_plot/static/js/dirty-forms.js
@@ -1,25 +1,56 @@
 /**
+ * This file contains convenience functions for working the the Dirty Forms JQuery plugin, which keeps track if form
+ * data has been modified, and warns the user before leaving the page if this is the case
  * @file dirty-forms.js
  * @date 2025-02-14
  * @author Bryan Gillis
  */
 
-export function initDirtyForms() {
-  $("form").dirtyForms();
+/**
+ * Initializes dirty forms checking for the current page
+ * @param {string} selector CSS selector for the elements to apply this to, default "form"
+ */
+export function initDirtyForms(selector = "form") {
+  $(selector).dirtyForms();
 }
 
-export function cleanDirtyForms() {
-  $('form').dirtyForms('setClean');
+/**
+ * Sets the current state of the page as "clean" with respect to dirty forms checking
+ * @param {string} selector CSS selector for the elements to apply this to, default "form"
+ */
+export function cleanDirtyForms(selector = "form") {
+  $(selector).dirtyForms('setClean');
 }
 
-export function dirtyDirtyForms() {
-  $('form').dirtyForms('setDirty');
+/**
+ * Sets the current state of the page as "dirty" with respect to dirty forms checking
+ * @param {string} selector CSS selector for the elements to apply this to, default "form"
+ */
+export function dirtyDirtyForms(selector = "form") {
+  $(selector).dirtyForms('setDirty');
 }
 
-export function enableDirtyForms() {
-  $('form').removeClass($.DirtyForms.ignoreClass);
+/**
+ * Enable dirty forms for the page if previously disabled
+ * @param {string} selector CSS selector for the elements to apply this to, default "form"
+ */
+export function enableDirtyForms(selector = "form") {
+  $(selector).removeClass($.DirtyForms.ignoreClass);
 }
 
-export function disableDirtyForms() {
-  $('form').addClass($.DirtyForms.ignoreClass);
+/**
+ * Disable dirty forms for the page
+ * @param {string} selector CSS selector for the elements to apply this to, default "form"
+ */
+export function disableDirtyForms(selector = "form") {
+  $(selector).addClass($.DirtyForms.ignoreClass);
+}
+
+/**
+ * Check if the forms on page are currently dirty or not
+ * @param {string} selector CSS selector for the elements to apply this to, default "form"
+ * @returns {boolean}
+ */
+export function checkIsDirty(selector = "form") {
+  return $(selector).dirtyForms('isDirty');
 }
