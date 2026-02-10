82174477b0a40dbc8ff958ce559c909a2262aba1
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2026-02-10 14:59:59 +0000

    Pass env vars to render_static

--- psdi_glorius_plot/render_static.py
+++ psdi_glorius_plot/render_static.py
@@ -7,9 +7,10 @@ import os
 import shutil
 from argparse import ArgumentParser
 
-import psdi_glorius_plot
 from flask import render_template
-from psdi_glorius_plot.gui.env import update_env
+
+import psdi_glorius_plot
+from psdi_glorius_plot.gui.env import get_env_kwargs, update_env
 from psdi_glorius_plot.gui.get import d_pages
 from psdi_glorius_plot.gui.setup import get_app
 
@@ -89,7 +90,7 @@ def main():
             qualified_path = os.path.join(output_dir, stripped_path)
 
             # Fix any relative links in the page, since the structure here will be a bit different
-            page_content = render_template(stripped_path)
+            page_content = render_template(stripped_path, **get_env_kwargs())
             page_content = page_content.replace("../", "./")
 
             open(qualified_path, "w").write(page_content)
